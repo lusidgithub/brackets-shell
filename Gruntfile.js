289716495680e8c7cad434e26f0b9acf7a036bdd
lusidgithub <rob@lusid.co.uk>
2014-01-08 19:07:47 +0000

    Revert "Update Gruntfile.js"
    This reverts commit 3ca2c318c017cfcf869a6d72b16c4b815a58b5ea.

--- Gruntfile.js
+++ Gruntfile.js
@@ -192,7 +192,7 @@ module.exports = function (grunt) {
         },
         "git": {
             "www": {
-                "repo"      : "../app",    // TODO user configurable?
+                "repo"      : "../brackets",    // TODO user configurable?
                 "branch"    : grunt.option("www-branch") || ""
             },
             "shell": {
